d9c8166e37fd234c8c151203a00e99d99c2b470c
Breezekid <calebbaraka79@gmail.com>
2023-11-16 22:38:18 +0300

    mandatory

---
+++ stack.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+#include <string.h>
+
+void free_stack(stack_t **stack);
+int init_stack(stack_t **stack);
+int check_mode(stack_t *stack);
+
+/**
+ * free_stack - Frees a stack_t stack.
+ * @stack: A pointer to the top (stack) or
+ *         bottom (queue) of a stack_t.
+ */
+void free_stack(stack_t **stack)
+{
+	stack_t *tmp = *stack;
+
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+}
